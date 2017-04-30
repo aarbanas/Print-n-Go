24f66e5c52995f809cf604a1efa1e206231d323d
Luka <lstimac@riteh.hr>
2017-04-30 10:48:41 +0200

    Napravio sam onaj activity gdje se otvara google maps, napravio da odmah po otvaranju mape se pokaže naša lokacija i stavi marker. Postavio sam action bar i dodao menu opciju na njega u slučaju da ćemo koristiti to za otvaranje lista i slicno.

--- app/src/main/java/com/example/arbi/printngo/MainActivity.java
+++ app/src/main/java/com/example/arbi/printngo/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.arbi.printngo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    public static final String EXTRA_MESSAGE = "com.arbi.printngo.MESSAGE";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
