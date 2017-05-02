096abaf395dd4ce7d66d1479c6e18f009fa04afd
aarbanas <aarbanas@riteh.hr>
2017-05-02 12:36:50 +0200

    Dodao sam uploada file-a

--- app/src/main/java/com/example/arbi/printngo/ListActivity.java
+++ app/src/main/java/com/example/arbi/printngo/ListActivity.java
@@ -1,13 +1,41 @@
 package com.example.arbi.printngo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ListActivity extends AppCompatActivity {
+
+    private Intent loadIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        Button openFile = (Button)findViewById(R.id.buttonSendFile);
+        openFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                loadIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                loadIntent.setType("*/*");
+                loadIntent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                try {
+
+                    startActivityForResult(Intent.createChooser(loadIntent, "Select your phrases .txt document"), 0);
+
+                } catch (android.content.ActivityNotFoundException e) {
+
+                    //if the user doesen't have file explorer installed
+                    Toast.makeText(ListActivity.this, "Please install a file manager!", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 }
