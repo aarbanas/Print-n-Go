fd2eae8cc2762b4a35e07d3053d32109b0056c8d
Luka <lstimac@riteh.hr>
2017-05-03 00:12:36 +0200

    Dodano je da ti kod odabira file-a izlista u recent samo word,txt,pdf,csv, a u downloads i sl. onemoguci biranje neceg sto nije jedan od tih formata. I napravio sam da se u onaj edit text sprema samo file name, file path ce ici u varijablu samo.

--- app/src/main/java/com/example/arbi/printngo/ListActivity.java
+++ app/src/main/java/com/example/arbi/printngo/ListActivity.java
@@ -1,12 +1,15 @@
 package com.example.arbi.printngo;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +22,7 @@ import android.widget.Toast;
 public class ListActivity extends AppCompatActivity {
 
     final int ACTIVITY_CHOOSE_FILE = 1;
+    private static final String TAG = "ListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +37,10 @@ public class ListActivity extends AppCompatActivity {
                 Intent chooseFile;
                 Intent intent;
                 chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
+                String [] mimeTypes = {"text/csv", "application/pdf","text/plain","application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/msword"};
                 chooseFile.setType("*/*");
+                chooseFile.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+
                 intent = Intent.createChooser(chooseFile, "Choose a file");
                 startActivityForResult(intent, ACTIVITY_CHOOSE_FILE);
             }
@@ -42,13 +49,37 @@ public class ListActivity extends AppCompatActivity {
     /*The selected file is than written in string (its path, not whole file) */
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         EditText editTextFile = (EditText) findViewById(R.id.editTextFilePath);
+
         switch(requestCode) {
             case ACTIVITY_CHOOSE_FILE: {
                 if (resultCode == RESULT_OK){
                     Uri uri = data.getData();
-                    String filePath = uri.getPath();
-                    editTextFile.setText(filePath, TextView.BufferType.EDITABLE);
+                    String filePath = uri.toString();
+                    File myFile = new File(filePath);
+                    String displayName = null;
+                    //editTextFile.setText(filePath, TextView.BufferType.EDITABLE);
+
+                    if (filePath.startsWith("content://")) {
+                    Cursor cursor = null;
+
+                    try {
+                        cursor = this.getContentResolver().query(uri, null, null, null, null);
+                        if (cursor != null && cursor.moveToFirst()) {
+                            displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                        }
+
+
+                    } finally {
+                        cursor.close();
+                    }
+                } else if (filePath.startsWith("file://")) {
+                    displayName = myFile.getName();
                 }
+
+                    editTextFile.setText(displayName, TextView.BufferType.EDITABLE);
+
+                    Log.i(TAG, uri.getPath());
+            }
             }
         }
     }
