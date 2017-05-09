e13c6ee600eea08ab8f370a1dd54651dc8d46daa
aarbanas <aarbanas@riteh.hr>
2017-05-09 09:06:33 +0200

    Page count of PDF files added

--- app/src/main/java/com/example/arbi/printngo/PrintActivity.java
+++ app/src/main/java/com/example/arbi/printngo/PrintActivity.java
@@ -16,6 +16,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.pdf.PdfRenderer;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Environment;
@@ -35,10 +36,12 @@ import android.widget.Toast;
 import com.shockwave.pdfium.PdfiumCore;
 
 
+
 public class PrintActivity extends AppCompatActivity {
 
     final int ACTIVITY_CHOOSE_FILE = 1;
     private static final String TAG = "PrintActivity";
+    public final static String FOLDER = Environment.getExternalStorageDirectory() + "/PDF";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,12 +66,11 @@ public class PrintActivity extends AppCompatActivity {
             }
         });
     }
-    /*The selected file's name is written in string.  */
+    /*The selected file's name is written in string. First page of selected PDF file is shown on image view */
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         EditText editTextFile = (EditText) findViewById(R.id.editTextFilePath);
-
-
+        TextView textViewShowData = (TextView) findViewById(R.id.textViewShowData);
         switch(requestCode) {
             case ACTIVITY_CHOOSE_FILE: {
                 if (resultCode == RESULT_OK){
@@ -77,7 +79,7 @@ public class PrintActivity extends AppCompatActivity {
                     File myFile = new File(filePath);
                     String displayName = null;
                     //editTextFile.setText(filePath, TextView.BufferType.EDITABLE);
-
+                    int count = 0;
                     if (filePath.startsWith("content://")) {
                     Cursor cursor = null;
 
@@ -93,18 +95,23 @@ public class PrintActivity extends AppCompatActivity {
                 } else if (filePath.startsWith("file://")) {
                     displayName = myFile.getName();
                 }
-                    editTextFile.setText(displayName, TextView.BufferType.EDITABLE);
-                    Log.i(TAG, uri.getPath());
-                    generateImageFromPdf(uri);
+                        editTextFile.setText(displayName, TextView.BufferType.EDITABLE);
+                        Log.i(TAG, uri.getPath());
+                    //Generating image and counting page numbers of PDF file
+                        generateImageFromPdf(uri);
 
-            }
+                }
             }
         }
 
     }
 
+    //PdfiumAndroid (https://github.com/barteksc/PdfiumAndroid)
+    //https://github.com/barteksc/AndroidPdfViewer/issues/49
+    //Generate image from first page, and count number of pages (only for PDF files)
     void generateImageFromPdf(Uri pdfUri) {
         ImageView showPdf = (ImageView) findViewById(R.id.imageViewShowPDF);
+        TextView textViewShowData = (TextView) findViewById(R.id.textViewShowData);
         int pageNumber = 0;
         PdfiumCore pdfiumCore = new PdfiumCore(this);
         try {
@@ -118,13 +125,14 @@ public class PrintActivity extends AppCompatActivity {
             pdfiumCore.renderPageBitmap(pdfDocument, bmp, pageNumber, 0, 0, width, height);
             saveImage(bmp);
             showPdf.setImageBitmap(bmp);
+            int pageCount = pdfiumCore.getPageCount(pdfDocument);
+            textViewShowData.setText("Broj stranica: " + Integer.toString(pageCount));
             pdfiumCore.closeDocument(pdfDocument); // important!
         } catch(Exception e) {
             //todo with exception
         }
     }
 
-    public final static String FOLDER = Environment.getExternalStorageDirectory() + "/PDF";
     private void saveImage(Bitmap bmp) {
         FileOutputStream out = null;
         try {
@@ -146,10 +154,20 @@ public class PrintActivity extends AppCompatActivity {
         }
     }
 
-/*Icon send file to copy shop is set in the menu */
+    public int countPages (Uri uriCountPdf){
+        int count = 0;
+
+
+        return count;
+    }
+
+
+    /*Icon send file to copy shop is set in the menu */
     public boolean onCreateOptionsMenu (Menu menu) {
         getMenuInflater().inflate(R.menu.print_activity_menu, menu);
 
         return true;
     }
+
+
 }
